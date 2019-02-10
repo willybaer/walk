d54faf2ce60edcda00ab7713e813248f29cb79b5
Wilhelm Dewald <wilhelmdewald@googlemail.com>
2019-02-10 03:16:28 +0100

    Now supporting seeds. Use comman "-w" to create new seeds and "-s" to execute seeds

--- setup.py
+++ setup.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python
 import setuptools
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name='walk',
-    version='0.2',
+    version='0.2.3',
     author='Wilhelm Dewald',
-    description='My own database migration and seeds tool for postgres by using psycopg2 with python3.',
+    description='PostgreSQL. Database migration and seeds tool for postgres by using psycopg2 with python3.',
+    long_description=long_description,
     long_description_content_type='text/markdown',
+    url='https://github.com/willybaer/walk',
+    packages=setuptools.find_packages(),
     install_requires=[
         'psycopg2'
         ],
     entry_points={
         'console_scripts': [
-            'walk=walk:main'
+            'walk=walk.walk:main'
         ],
-    }
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
 )
--- walk/walk.py
+++ walk/walk.py
@@ -6,7 +6,7 @@ import json
 import psycopg2
 import traceback
 
-from argsextractor import filter_args
+from walk.argsextractor import filter_args
 from psycopg2.extensions import AsIs
 from psycopg2.extras import DictCursor
 
@@ -27,9 +27,15 @@ EXAMPLE_CONFIG = {
 }
 ALLOWED_CONFIG_PARAMS = ['user', 'dbname', 'password', 'host', 'port']
 DB_CHANGE_LOG = 'database_change_log'
+DB_LOG_SEEDS = 'database_seeds_log'
 MIGRATIONS_DIR = 'migrations'
 CONFIG_FILE_NAME = 'walk_config.json'
 
+def connection(db_config):
+    return psycopg2.connect(' '.join(db_config))
+
+def cursor(con):
+    return con.cursor(cursor_factory=DictCursor)
 
 def generate_example_config(directory, force=False):
     # Check if the config file is existing
@@ -58,41 +64,35 @@ def generate_example_config(directory, force=False):
     # Creating folder
     os.makedirs(migrations_dir)    
 
-def new(file_name, migrations_dir):
-    # Creating migrations folder if not exists
-    if not os.path.exists(migrations_dir):
-        os.makedirs(migrations_dir)
+def new(file_name, t_dir, task='migrations'):
+    # Creating migrations/seeds folder if not exists
+    if not os.path.exists(t_dir):
+        os.makedirs(t_dir)
 
     # Create new file with current timestamp
     d_file_name = '%s_%s.sql' % (datetime.datetime.now().strftime('%Y%m%d%H%M%S'), file_name)
-    open('%s/%s' % (migrations_dir, d_file_name), 'w')
-    print('Created new migrations file %s' % d_file_name)    
+    open('%s/%s' % (t_dir, d_file_name), 'w')
+    print('Created new %s file %s' % (task, d_file_name))        
 
-def connection(db_config):
-    return psycopg2.connect(' '.join(db_config))
-
-def cursor(con):
-    return con.cursor(cursor_factory=DictCursor)    
-
-def migrate(env, db_config, migrations_dir):
-    # Migrations folder
-    if os.path.exists(migrations_dir):
-        # Run migrations
+def run(env, db_config, t_dir, task='migration', db_log=DB_CHANGE_LOG):
+    # Task folder
+    if os.path.exists(t_dir):
+        # Open connection and create a cursor
         conn = connection(db_config=db_config)
         cur = cursor(con=conn)
 
         # Check if change log table exists
         cur.execute('select exists(select * from information_schema.tables where table_name=%s)',
-                    (DB_CHANGE_LOG,))
+                    (db_log,))
         if not cur.fetchone()[0]:
             print('creating database change log table')
-            cur.execute('CREATE TABLE %s (id serial primary key, last_timestamp bigint)', (AsIs(DB_CHANGE_LOG),))
+            cur.execute('CREATE TABLE %s (id serial primary key, last_timestamp bigint)', (AsIs(db_log),))
             conn.commit()
         else:
             print('database changelog table already exists')
 
-        # Iterate over migration files
-        files = os.listdir(migrations_dir)
+        # Iterate over task files
+        files = os.listdir(t_dir)
         files = list(filter(lambda f: f.endswith('.sql') , files))
         ordered_files = sorted(files, key=lambda x: int(x.split('_')[0]))
         try:
@@ -103,21 +103,21 @@ def migrate(env, db_config, migrations_dir):
                     log_level = log_level[0]
 
                     # Check if migration exists
-                    cur.execute('SELECT * FROM %s ORDER BY last_timestamp DESC', (AsIs(DB_CHANGE_LOG),))
+                    cur.execute('SELECT * FROM %s ORDER BY last_timestamp DESC', (AsIs(db_log),))
                     last_entry = cur.fetchone()
 
                     if last_entry and last_entry['last_timestamp'] >= int(log_level):
-                        print('Migration already exists for file %s' % file)
+                        print('%s already exists for file %s' % (task, file))
                     else:
                         # Execute sql
-                        print('executing migration for file %s' % file)
-                        s = open('%s/%s' % (migrations_dir, file), 'rb').read().decode('UTF-8')
+                        print('executing %s for file %s' % (task, file))
+                        s = open('%s/%s' % (t_dir, file), 'rb').read().decode('UTF-8')
                         cur.execute(s)
                         conn.commit()
 
                         # Add migration log entry
                         print('Adding log entry for timestamp: %s' % log_level)
-                        cur.execute('INSERT INTO %s (last_timestamp) VALUES (%s)', (AsIs(DB_CHANGE_LOG), int(log_level),))
+                        cur.execute('INSERT INTO %s (last_timestamp) VALUES (%s)', (AsIs(db_log), int(log_level),))
                         conn.commit()
         except Exception as e:
             print(e)
@@ -129,19 +129,23 @@ def migrate(env, db_config, migrations_dir):
             if conn is not None:
                 conn.close()
     else:
-        print('Migrations folder does not exists: %s' % migrations_dir)
+        print('%s folder does not exists: %s' % (task, t_dir))
 
 def main():
     argv = sys.argv
-    args = filter_args(argv=argv, opts=[('n:', 'new='), ('m', 'migrate'), ('e:', 'env='), ('i', 'init'), ('f', 'force'), ('p:', 'param=')])
-
+    args = filter_args(argv=argv, opts=[('n:', 'new='), ('m', 'migrate'), 
+            ('e:', 'env='), ('i', 'init'), ('f', 'force'), ('p:', 'param='),
+            ('w:', 'newseed='), ('s', 'seeds')])
+    
     # Print information
-    if 'new=' not in args.keys() and 'migrate' not in args.keys() and 'init' not in args.keys():
+    if len(list(map(lambda k: k in ('new=', 'migrate', 'init', 'newseed=', 'seeds'), args.keys()))) == 0:
         print(  '' +
                 '-i or --init                   Generates a default config file and the migrations directory. \n' +
                 '-f or --force                  Forces the init function to recreate the default settings. \n' +
                 '-n or --new test_abc           Creates a new migration file. \n' +
+                '-w or --newseed test_abc     Creates a new seeds file. \n' +
                 '-m or --migrate                Runs the migrations. \n' +
+                '-s or --seed                   Runs the seeds. \n' +
                 '-e or --env test               Database environment which will be chosen for the migrations. \n' +
                 ('-p or --param dbname=db        Pass database connections parameters dynamically. Valid attributes: %s \n' % ALLOWED_CONFIG_PARAMS) +
                 '')
@@ -157,10 +161,17 @@ def main():
     migrations_directory = './migrations' # Default
     if 'new=' in args.keys():
         new_file = args['new='][0]
-        new(file_name=new_file, migrations_dir=migrations_directory)
+        new(file_name=new_file, t_dir=migrations_directory)
         return
+
+    ## New seeds file
+    seeds_directory = './seeds' # Default
+    if 'newseed=' in args.keys():
+        new_file = args['newseed='][0]
+        new(file_name=new_file, t_dir=seeds_directory, task='seeds')
+        return    
     
-    ## MIGRATIONS
+    ## MIGRATIONS OR/AND SEEDS
     if not os.path.exists('./%s' % CONFIG_FILE_NAME):
         print('Missing config file. Use \'walk init\' to generate a config file and a migrations folder')
         return
@@ -186,7 +197,10 @@ def main():
     config_file = list(filter(lambda e: e.split('=')[0] not in config_params_to_merge, config))
     config = config_to_merge + config_file
     if 'migrate' in args.keys():
-        migrate(env, migrations_dir=migrations_directory, db_config=config)    
+        run(env, t_dir=migrations_directory, db_config=config, task='migration', db_log=DB_CHANGE_LOG)
+
+    if 'seeds' in args.keys():
+        run(env, t_dir=seeds_directory, db_config=config, task='seed', db_log=DB_LOG_SEEDS)        
 
 if __name__ == '__main__':
     main()            
